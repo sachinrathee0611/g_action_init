deab6481fece417ab35613bd976e15eca48707d4
sachinrathee0611 <132877288+sachinrathee0611@users.noreply.github.com>
2025-02-23 15:28:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CI Pipeline by Sachin!"
+    return "Hello, CI Pipeline by Sachin Rathee in PW Skills LIVE Class!"
 
 if __name__ == "__main__":
     app.run(debug=True)
